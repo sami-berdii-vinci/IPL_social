415ac4bf296ecc73df5b0af3be41eda6726d173d
S-Berdii <sami.berdii@student.vinci.be>
2025-01-17 17:15:37 +0100

    Refactor: checkSpecialCharacters function

--- functions/passwordChecker.js
+++ functions/passwordChecker.js
@@ -1,12 +1,16 @@
 function isValid(password){
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-        return false;
-    }
-    return checkPasswordLength(password);
+    
+    return checkPasswordLength(password) 
+    && checkSpecialCharacters(password);
 }
 
 function checkPasswordLength(password){
     if(password.length < 8) return false;
+    return true;
+}
+
+function checkSpecialCharacters(password){
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false;
     return true;
 }
 
